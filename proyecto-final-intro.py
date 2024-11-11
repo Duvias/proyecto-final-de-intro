bf9bf04b30f70a7fa832eab63713df82e08fed66
Duvias <125936640+Duvias@users.noreply.github.com>
2024-11-11 00:42:01 -0500

    Update proyecto-final-intro.py

--- proyecto-final-intro.py
+++ proyecto-final-intro.py
@@ -1,1 +1,14 @@
+def atencion():
+    while True:
+        try:
+            accion=int(input("(1) Atención en Mesa\n(2) Comprar para llevar\nEscoge la acción a realizar escribiendo 1 o 2: "))
+            if accion == 1 or accion == 2:
+                break
+        except(ValueError):
+            print("\nPor favor digita numeros unicamente")
+    
+    if accion == 1:
+        print("Menú--------------------------\n")
 
+
+atencion()
